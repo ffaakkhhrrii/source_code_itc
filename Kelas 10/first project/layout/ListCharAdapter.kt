955f1bbef9c7e13eafd954b66889866f8b9950b8
fakhri-git <test@fakhri.com>
2024-01-15 19:18:57 +0700

    add listCharAdapter.kt dan update ProjectActivity.kt

---
+++ Kelas 10/first project/layout/ListCharAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListCharAdapter():RecyclerView.Adapter<ListCharAdapter.MyViewHolder>(){
+    
+    class MyViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
+
--- Kelas 10/first project/layout/ProjectActivity.kt
+++ Kelas 10/first project/layout/ProjectActivity.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class ProjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project)
+
     }
 }
