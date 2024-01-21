1e36251e41b2309e0f813f36e9ca9dc50366e7a8
fakhri-git <test@fakhri.com>
2024-01-21 19:08:30 +0700

    update adapter and ProjectActivity.kt

--- Kelas 10/first project/layout/ListCharAdapter.kt
+++ Kelas 10/first project/layout/ListCharAdapter.kt
@@ -1,25 +1,38 @@
 package com.example.myapplication
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class ListCharAdapter():RecyclerView.Adapter<ListCharAdapter.MyViewHolder>(){
+class ListCharAdapter(private val listChar: ArrayList<Character>):RecyclerView.Adapter<ListCharAdapter.MyViewHolder>(){
     
     class MyViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
-
+        val imageChar: ImageView = itemView.findViewById(R.id.movieImage)
+        val nameChar: TextView = itemView.findViewById(R.id.nameCharacter)
+        val descriptionChar: TextView = itemView.findViewById(R.id.descCharacter)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_char_item,parent,false)
+        return MyViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listChar.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.nameChar.text = listChar[position].name
+        holder.descriptionChar.text = listChar[position].description
+
+        Glide.with(holder.itemView.context)
+            .load(listChar[position].image)
+            .into(holder.imageChar)
+
     }
 
 }
--- Kelas 10/first project/layout/ProjectActivity.kt
+++ Kelas 10/first project/layout/ProjectActivity.kt
@@ -6,11 +6,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ProjectActivity : AppCompatActivity() {
+
+    private lateinit var rv : RecyclerView
+    private val list : ArrayList<Character> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project)
+
+        list.addAll(DataCharacter.listChar)
+        rv = findViewById(R.id.myRv)
+        rv.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this)
+        rv.layoutManager = layoutManager
+        val adapter = ListCharAdapter(list)
+        rv.adapter = adapter
 
     }
 }
